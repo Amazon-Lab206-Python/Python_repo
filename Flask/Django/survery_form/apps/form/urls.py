09ef5a471c7c8afa4c674bbde4fb17ad1661f6f4
Hendry <noahendr@amazon.com>
2017-11-14 10:16:40 -0700

    Deploy move

--- Flask/Django/survery_form/apps/form/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^results$', views.results),
-    url(r'^surveys/process$', views.process)
-]
